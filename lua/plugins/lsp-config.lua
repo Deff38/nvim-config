212762ee8d4fa9c62fb40ee223d351aef08c7d04
Deff38 <giovanni.defilippo@proton.me>
2025-04-28 23:22:05 +0200

    Added autocompletion and snippets

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,31 +1,83 @@
 return {
-  {
-  -- mason
-  "williamboman/mason.nvim",
-  config = function()
-    require("mason").setup()
-  end
-  },
-  {
-  -- mason-lspconfig
-  "williamboman/mason-lspconfig.nvim",
-  config = function()
-    require("mason-lspconfig").setup({
-      ensure_installed = { "lua_ls", "ts_ls", "rust_analyzer", "bashls", "chsarp_ls", "cssls", "harper_ls", "html", "jsonls", "hyprls", "java_language_server", "htmx", "ast-grep" },
-    })
-  end
-  },
-  {
-  -- lspconfig
-  "neovim/nvim-lspconfig",
-  config = function()
-    local lspconfig = require("lspconfig")
-    lspconfig.lua_ls.setup({})
-    lspconfig.ts_ls.setup({})
+	{
+		-- mason
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-    vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action, {})
-  end
-  }
+	{
+		-- mason-lspconfig
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"ts_ls",
+					"rust_analyzer",
+					"bashls",
+					"csharp_ls",
+					"cssls",
+					"html",
+					"jsonls",
+					"hyprls",
+					"java_language_server",
+					"htmx",
+					"ast_grep",
+				},
+			})
+		end,
+	},
+	{
+		-- lspconfig
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.csharp_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jsonls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.hyprls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.java_language_server.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.htmx.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ast_grep.setup({
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
+
+
